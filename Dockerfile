fed3abebd4be61ffde6e8e3a282fe9ca94d9488e
gleal42 <gonaloleal@gmail.com>
2021-04-08 00:13:09 +0100

    added Mariadb/mysql

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@
 #    By: gleal <gleal@student.42lisboa.com>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/05 23:24:47 by gleal             #+#    #+#              #
-#    Updated: 2021/04/07 18:32:30 by gleal            ###   ########.fr        #
+#    Updated: 2021/04/07 21:54:08 by gleal            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,6 +16,7 @@ RUN apt-get update
 RUN apt-get upgrade -y
 RUN apt-get install -y nginx
 RUN apt-get install openssl
+RUN apt-get -y install mariadb-server
 
 COPY /srcs/nginx/server.conf /etc/nginx/sites-available/server.conf
 RUN ln -s /etc/nginx/sites-available/server.conf /etc/nginx/sites-enabled/server.conf
--- srcs/start.sh
+++ srcs/start.sh
@@ -6,7 +6,7 @@
 #    By: gleal <gleal@student.42lisboa.com>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/07 00:26:43 by gleal             #+#    #+#              #
-#    Updated: 2021/04/07 18:20:14 by gleal            ###   ########.fr        #
+#    Updated: 2021/04/07 23:41:49 by gleal            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,6 @@ chown -R www-data /var/www/*
 chmod -R 755 /var/www/*
 
 mkdir /etc/nginx/ssl
-
 openssl req -newkey rsa:4096 \
 			-x509 \
 			-sha256 \
@@ -24,8 +23,16 @@ openssl req -newkey rsa:4096 \
 			-keyout /etc/nginx/ssl/localhost.key \
 			-subj "/C=PT/ST=Lisbon/L=Lisbon/O=42 Lisboa/OU=gleal/CN=www.localhost.com"
 
-service nginx start
+service mysql start
+mysql -u root --skip-password << HERE
+CREATE DATABASE database_wp;
+GRANT ALL PRIVILEGES ON database_wp.* TO 'root'@'localhost';
+FLUSH PRIVILEGES;
+SHOW DATABASES
+HERE
+service mysql status
 
+service nginx start
 nginx -t
 service nginx status
 
